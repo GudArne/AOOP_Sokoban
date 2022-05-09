ca1dff63cee640934f757b6db4b4ee8b0e8e95cd
Mattias <mattias9922@hotmail.com>
2022-05-09 11:09:58 +0200

    Added DataModel

--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -1,8 +1,24 @@
 package Main;
 
+import javax.swing.*;
+
 public class GameWindow {
+    
+
+    public GameWindow(JFrame window, GamePanel gamePanel){
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Sokoban");
+
+        window.add(gamePanel);
+
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
 
     public static void main(String[] args) {
         System.out.println("test");
+        GameWindow gameWindow = new GameWindow(new JFrame(), new GamePanel());
     }
 }
